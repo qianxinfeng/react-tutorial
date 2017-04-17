fdad92183980308d43ac7cd8ac49040331158aad
xinfeng_qian <xinfeng_qian@qq.com>
2017-04-17 23:25:21 +0800

    update to use es6 define component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import TodoList from './TodoList.js';
-var App = React.createClass({
-  getInitialState(){
-    return {items:["123","xxxx"],text:""};
-  },
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:["123","xxxx"],
+      text:""
+    }
+  }
   onChange(e){
     this.setState({text:e.target.value});
-  },
+  }
   handleSubmit(e){
     e.preventDefault();
     let text=this.state.text;
@@ -15,24 +19,24 @@ var App = React.createClass({
     }
     var nextItems=this.state.items.concat([text]);
     this.setState({items:nextItems,text:""});
-  },
+  }
   handleDelete(index){
     let nextItems=this.state.items;
     nextItems.splice(index,1);
     this.setState({items:nextItems});
-  },
+  }
   render(){
     return (
       <div >
         <h3>TODO</h3>
-        <TodoList items={this.state.items} onDelete={this.handleDelete}/>
-        <form onSubmit={this.handleSubmit}>
-           <input onChange={this.onChange} value={this.state.text} />
+        <TodoList items={this.state.items} onDelete={this.handleDelete.bind(this)}/>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+           <input onChange={this.onChange.bind(this)} value={this.state.text} />
            <button>{'Add #' + (this.state.items.length + 1)}</button>
         </form>
       </div>
     );
   }
-});
+}
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-var TodoList = React.createClass({
+class TodoList extends React.Component{
     render() {
         var createItem = (itemText, index) => {
             return <li key={index}>{itemText}<button onClick={e=>this.props.onDelete(index)}>x</button></li>
         };
         return <ul>{this.props.items.map(createItem)}</ul>;
     }
-});
+}
 export default TodoList;
