f7ba8d9673fa9b4ddd17385995e9118f8ce7ae62
xinfeng_qian <xinfeng_qian@hansight.com>
2017-04-17 17:35:06 +0800

    add delete item method

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoList from './TodoList.js';
 var App = React.createClass({
   getInitialState(){
-    return {items:[],text:""};
+    return {items:["123","xxxx"],text:""};
   },
   onChange(e){
     this.setState({text:e.target.value});
@@ -16,11 +16,16 @@ var App = React.createClass({
     var nextItems=this.state.items.concat([text]);
     this.setState({items:nextItems,text:""});
   },
+  handleDelete(index){
+    let nextItems=this.state.items;
+    nextItems.splice(index,1);
+    this.setState({items:nextItems});
+  },
   render(){
     return (
       <div >
         <h3>TODO</h3>
-        <TodoList items={this.state.items} />
+        <TodoList items={this.state.items} onDelete={this.handleDelete}/>
         <form onSubmit={this.handleSubmit}>
            <input onChange={this.onChange} value={this.state.text} />
            <button>{'Add #' + (this.state.items.length + 1)}</button>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import React from 'react';
 var TodoList = React.createClass({
     render() {
         var createItem = (itemText, index) => {
-            return <li key={index}>{itemText}</li>
+            return <li key={index}>{itemText}<button onClick={e=>this.props.onDelete(index)}>x</button></li>
         };
         return <ul>{this.props.items.map(createItem)}</ul>;
     }
